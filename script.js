d581caa1d7cb077516f9e7f6ad8c1a8393606531
PANKAJ SHARMA <147194794+Pankaj-Sharmaa@users.noreply.github.com>
2024-03-14 14:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function insert_Row() {
     //Write your code here
-	let table = document.getElementById();
+	let table = document.getElementById('sampleTable');
 	let btn = document.querySelector('input');
 
 	btn.addEventListener('click', addRow)
