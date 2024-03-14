980bd57731b7029c51d16a6e73628c9fcd130651
PANKAJ SHARMA <147194794+Pankaj-Sharmaa@users.noreply.github.com>
2024-03-14 14:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table = document.getElementById();
+	let btn = document.querySelector('input');
+
+	btn.addEventListener('click', addRow)
+
+   function addRow() {
+    let row = table.insertRow(0);
+	   row.insertCell(0).innerHTML = "New Cell 1";
+	   row.insertCell(1).innerHTML = "New Cell 2";
+   }
 }
