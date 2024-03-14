5a27e89de202799bb619d59985214528cfd9e0cd
PANKAJ SHARMA <147194794+Pankaj-Sharmaa@users.noreply.github.com>
2024-03-14 14:29:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ function insert_Row() {
     //Write your code here
 	let table = document.getElementById('sampleTable');
 
-	// btn.addEventListener('click', addRow)
-
-   function addRow() {
     let row = table.insertRow(0);
 	   row.insertCell(0).innerHTML = "New Cell 1";
 	   row.insertCell(1).innerHTML = "New Cell 2";
-   }
 }
